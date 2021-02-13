e7f3094568335f1ff7cf6159a9e68ce1520ffb74
dhruvsinghal34 <drmonikagupta16@gmail.com>
2021-02-13 16:06:10 +0530

    bar

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -1,24 +1,51 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-class AppHeader extends React.Component{
-  render(){
-    return(
-      <View style= {styles.textContainer}>
-        <Text style={styles.text}>BARTER APP SYSTEM </Text>
-      </View>
-    );
-  }
+import React from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyleSheet ,Alert} from 'react-native';
+import db from '../config'
+
+export default class AppHeader extends React.Component{
+ constructor(props){
+   super(props)
+   this.state={
+    value:"",
+   }
+ }
+ getNumberOfUnreadNotifications=()=>{
+   db.collection("all_Notification").where("notification_status","==","unread")
+   .onSnapshot((snapshot)=>{
+     var unreadNotification=snapshot.docs.map((doc)=>doc.data())
+     this.setState({
+       value:unreadNotification.length
+     })
+   })
+ }
+ componentDidMount(){
+   this.getNumberOfUnreadNotifications()
+ }
 }
-const styles = StyleSheet.create({
-  textContainer:{
-    backgroundColor: 'green',
-  },
-  text:{
-    color: 'white',
-    padding: 20,
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
-});
+
+const BellIconWithBadge =(props)=>{
+ return(
+   <View>
+     <Icon name='bell' type='font-awesome' color='#696969' size={25}
+     onPress={()=>props.navigation.navigate('Notification')}></Icon>
+     <Badge 
+     value={this.state.value}
+     containerStyle={{position:'absolute',top:-4,right:-4}}
+     ></Badge>
+   </View>
+ )
+}
+
+const AppHeader = props => {
+  return (
+    <Header
+      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
+      centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+      rightComponent={<BellIconWithBadge {...props}></BellIconWithBadge>}
+      backgroundColor = "#eaf8fe"
+    />
+  );
+};
+
 export default AppHeader;
